735b8eb84269996ec610bf6f22df41ec556d0f6f
github-action-benchmark <github@users.noreply.github.com>
2022-12-15 10:20:18 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 27034a1865e0f3aa9cd95b8f6a4707fab82c1e3e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671052507449,
+  "lastUpdate": 1671099618511,
   "repoUrl": "https://github.com/Kha/nale",
   "entries": {
     "Benchmark": [
@@ -122,6 +122,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "URL build",
             "value": "26.32",
+            "unit": "s"
+          },
+          {
+            "name": "URL build [RX]",
+            "value": "1",
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sebasti@nullri.ch",
+            "name": "Sebastian Ullrich",
+            "username": "Kha"
+          },
+          "committer": {
+            "email": "sebasti@nullri.ch",
+            "name": "Sebastian Ullrich",
+            "username": "Kha"
+          },
+          "distinct": true,
+          "id": "27034a1865e0f3aa9cd95b8f6a4707fab82c1e3e",
+          "message": "update nix-portable, actually eliminating git dependency",
+          "timestamp": "2022-12-15T10:58:52+01:00",
+          "tree_id": "be8edb7de227a834e0f5bec01a12366b5204ba36",
+          "url": "https://github.com/Kha/nale/commit/27034a1865e0f3aa9cd95b8f6a4707fab82c1e3e"
+        },
+        "date": 1671099617243,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Initial build",
+            "value": "901.35",
+            "unit": "s"
+          },
+          {
+            "name": "Initial build [RX]",
+            "value": "626",
+            "unit": "MB"
+          },
+          {
+            "name": "No-op rebuild",
+            "value": "0.57",
+            "unit": "s"
+          },
+          {
+            "name": "No-op rebuild [RX]",
+            "value": "0",
+            "unit": "MB"
+          },
+          {
+            "name": "Rebuild after unrelated change",
+            "value": "44.71",
+            "unit": "s"
+          },
+          {
+            "name": "Rebuild after unrelated change [RX]",
+            "value": "0",
+            "unit": "MB"
+          },
+          {
+            "name": "URL build",
+            "value": "26.16",
             "unit": "s"
           },
           {
