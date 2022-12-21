24b6cf1ff4351d7cf106f651be846ba60606b068
github-action-benchmark <github@users.noreply.github.com>
2022-12-21 20:49:54 +0000

    add Lake Reference (customSmallerIsBetter) benchmark result for c73ee2fe0c0cae8f7b4c10a9e80b62d713fc464e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671560564136,
+  "lastUpdate": 1671655794675,
   "repoUrl": "https://github.com/Kha/nale",
   "entries": {
     "Benchmark": [
@@ -1852,6 +1852,60 @@ window.BENCHMARK_DATA = {
           },
           {
             "name": "No-op rebuild [RX]",
+            "value": "0",
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sebasti@nullri.ch",
+            "name": "Sebastian Ullrich",
+            "username": "Kha"
+          },
+          "committer": {
+            "email": "sebasti@nullri.ch",
+            "name": "Sebastian Ullrich",
+            "username": "Kha"
+          },
+          "distinct": true,
+          "id": "c73ee2fe0c0cae8f7b4c10a9e80b62d713fc464e",
+          "message": "add disk space metric",
+          "timestamp": "2022-12-21T21:43:09+01:00",
+          "tree_id": "24ee31e721e527019c82d074464c7f767cff6e0f",
+          "url": "https://github.com/Kha/nale/commit/c73ee2fe0c0cae8f7b4c10a9e80b62d713fc464e"
+        },
+        "date": 1671655793957,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Initial build",
+            "value": "85.96",
+            "unit": "s"
+          },
+          {
+            "name": "Initial build [RX]",
+            "value": "200",
+            "unit": "MB"
+          },
+          {
+            "name": "Initial build [disk]",
+            "value": "955",
+            "unit": "MB"
+          },
+          {
+            "name": "No-op rebuild",
+            "value": "0.39",
+            "unit": "s"
+          },
+          {
+            "name": "No-op rebuild [RX]",
+            "value": "0",
+            "unit": "MB"
+          },
+          {
+            "name": "No-op rebuild [disk]",
             "value": "0",
             "unit": "MB"
           }
