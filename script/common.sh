c73ee2fe0c0cae8f7b4c10a9e80b62d713fc464e
Sebastian Ullrich <sebasti@nullri.ch>
2022-12-21 20:17:38 +0100

    add disk space metric

--- script/common.sh
+++ script/common.sh
@@ -4,15 +4,19 @@ echo > times.csv
 root=$PWD
 bench() {
   bytes=$(ifconfig | sed -En 's/RX.*bytes ([0-9]+).*/\1/p' | sort -rn | head -n1)
+  fsmb=$(du -sm ~ | cut -f1)
   \time -f "$1,%e,%M" -ao $root/times.csv "${@:2}"
   bytes2=$(ifconfig | sed -En 's/RX.*bytes ([0-9]+).*/\1/p' | sort -rn | head -n1)
+  fsmb2=$(du -sm ~ | cut -f1)
   mb=$(( (bytes2-bytes) / 1000000 ))
-  sed -i "\$s/\$/,$mb/" $root/times.csv
+  fsdelta=$((fsmb2-fsmb))
+  sed -i "\$s/\$/,$mb,$fsdelta/" $root/times.csv
 }
 toJson() {
   jq -Rsn '[inputs | . / "\n" | (.[] | select(length > 0) | . / ",") as $input |
     [{"name": $input[0], "unit": "s", "value": $input[1] },
-    {"name": "\($input[0]) [RX]", "unit": "MB", "value": $input[3] }]] |
+     {"name": "\($input[0]) [RX]", "unit": "MB", "value": $input[3] },
+     {"name": "\($input[0]) [disk]", "unit": "MB", "value": $input[4] }]] |
     flatten' < $root/times.csv > $root/times.json
 }
 
